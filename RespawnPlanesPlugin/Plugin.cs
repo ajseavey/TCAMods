048f4b32ba4b339cc5ba10f63158157c1af177e4
Adam <admsvy@gmail.com>
2022-02-25 16:02:37 -0500

    Add files for plane respawning patches

---
+++ RespawnPlanesPlugin/Plugin.cs
@@ -0,0 +1,101 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Falcon.Game2.Arena2;
+using Falcon.UniversalAircraft;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace RespawnPlanesPlugin
+{
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Arena.exe")]
+    public class Plugin : BaseUnityPlugin
+    {      
+        static Dictionary<ArenaAirfieldFlight, UniFlight> ActiveFlights = new Dictionary<ArenaAirfieldFlight, UniFlight>();
+
+        static ManualLogSource Log;
+
+        [HarmonyPatch(typeof(StrategicTarget2))]
+        [HarmonyPatch("InitializeForArena")]
+        [HarmonyPrefix]
+        static bool InitializeForArena(ArenaStrategicTarget data, StrategicTarget2 __instance)
+        {
+            Log?.LogDebug("InitializeForArena Patch Called");
+            __instance.AirfieldData = data.Airfield;
+            return true;
+        }
+
+        [HarmonyPatch(typeof(StrategicTarget2))]
+        [HarmonyPatch("SpawnFlight")]
+        [HarmonyPostfix]
+        static void SpawnFlight(ArenaAirfieldFlight flightParams, UniFlight __result)
+        {
+            Log?.LogDebug("SpawnFlight Patch Called");
+            ActiveFlights[flightParams] = __result;
+        }
+
+        [HarmonyPatch(typeof(StrategicTarget2))]
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static bool Update(StrategicTarget2 __instance, ref float ___spawnCooldown)
+        {
+            if (!StrategicTarget2.IsArenaRunning || __instance.Faction == null)
+            {
+                return false;
+            }
+            ___spawnCooldown -= Time.deltaTime;
+
+            if (___spawnCooldown <= 0f && !__instance.IsTargetDestroyed)
+            {
+                foreach(StrategicFormationSpawn formation in __instance.OwnedFormations)
+                {
+                    var factionSpawnAllowed = !formation.IsFactionSpecific || !(formation.Faction != __instance.Faction.Name);
+                    if (factionSpawnAllowed && formation.IsSpawnAllowed) 
+                    {
+                        Log.LogDebug("Spawning Formation " + formation.ToString());
+                        typeof(StrategicTarget2).GetMethod("SpawnFormation", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(__instance, new object[] { formation });
+                    }
+                }
+                foreach (ArenaAirfieldFlight flight in __instance.AirfieldData.DefensiveFlights)
+                {
+                    var activeFlight = ActiveFlights[flight];
+                    if (!(flight.Faction != __instance.Faction.Name) && (activeFlight == null || activeFlight.IsDestroyed))
+                    {
+                        Log.LogDebug("Spawning Flight " + flight.ToString());
+                        typeof(StrategicTarget2).GetMethod("SpawnFlight", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(__instance, new object[] { flight });
+                    }
+                }
+                ___spawnCooldown = __instance.FormationSpawnTime;
+            }
+            return false;
+        }
+
+
+        [HarmonyPatch(typeof(StrategicTarget2))]
+        [HarmonyPatch("StopArenaLogic")]
+        [HarmonyPostfix]
+        static void StopArenaLogic()
+        {
+            Log?.LogDebug("StopArenaLogic Patch Called");
+            ActiveFlights.Clear();
+        }
+    
+
+        private void Awake()
+        {
+            Log = this.Logger;
+            if (!typeof(StrategicTarget2).GetMethod("SpawnFlight", BindingFlags.NonPublic | BindingFlags.Instance).ReturnType.Equals(typeof(UniFlight)))
+            {
+                Log.LogError("SpawnFlight not patched to return UniFlight. Unable to patch");
+                return;
+            }
+
+            Harmony harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
+            harmony.GetPatchedMethods().Do((method) => Log.LogDebug(method.ToString() + " Patched"));
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ SpawnFlightPatch/SpawnFlightPatch/SpawnFlightPatch.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using BepInEx.Logging;
+
+namespace SpawnFlightPatch
+{
+    public static class Patcher
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+        private static ManualLogSource logger = Logger.CreateLogSource("SpawnFlightPatch");
+
+        public static void Patch(AssemblyDefinition assembly)
+        {  
+            TypeDefinition StrategicTarget2 = assembly.MainModule.Types.Where(type => type.IsClass && type.Namespace == "Falcon.Game2.Arena2" && type.Name == "StrategicTarget2").First();
+            if (StrategicTarget2 != null)
+            {
+                logger.LogDebug("Found StrategicTarget2");
+                MethodDefinition SpawnFlight = StrategicTarget2.Methods.Where(method => method.Name == "SpawnFlight").First();
+
+                if (SpawnFlight != null)
+                {
+                    logger.LogDebug("Found SpawnFlight");
+                    TypeReference UniFlight = GetUniFlightType(assembly);
+                    if (SpawnFlight.ReturnType == UniFlight)
+                    {
+                        logger.LogInfo("Return Type Already Patched");
+                        return;
+                    }
+                    SpawnFlight.ReturnType = UniFlight;
+
+                    Instruction ret = SpawnFlight.Body.Instructions.Last();
+
+                    // Remove pop before ret in SpawnFlight;
+                    SpawnFlight.Body.GetILProcessor().Remove(ret.Previous);
+                }
+
+
+                MethodDefinition InitializeForArena = StrategicTarget2.Methods.Where(method => method.Name == "InitializeForArena").First();
+                if (InitializeForArena != null)
+                {
+                    logger.LogDebug("Found InitializeForArena");
+                    Instruction callSpawnFlight = InitializeForArena.Body.Instructions.Where(instruction => instruction.OpCode == OpCodes.Call && instruction.Operand == SpawnFlight.GetElementMethod()).First();
+                    InitializeForArena.Body.GetILProcessor().InsertAfter(callSpawnFlight, InitializeForArena.Body.GetILProcessor().Create(OpCodes.Pop));
+                }
+            }
+            else
+            {
+                logger.LogError("Unable to find StrategicTarget2");
+            }            
+            logger.LogInfo("Patch complete");
+        }
+
+        private static TypeReference GetUniFlightType(AssemblyDefinition assembly)
+        {            
+            TypeDefinition UniFlight = assembly.MainModule.Types.Where(type => type.IsClass && type.Namespace == "Falcon.UniversalAircraft" && type.Name == "UniFlight").First();
+            if (UniFlight != null)
+            {
+                logger.LogDebug("Found UniFlight");
+
+                return UniFlight.GetElementType();
+            }
+            else
+            {
+                logger.LogError("Unable to find UniFlight");
+            }
+            
+            return null;
+        }
+    }
+}
